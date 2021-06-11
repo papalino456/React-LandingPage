ff862c805c2ea5336dc502549cb02ac72c61aa3c
papalino456 <sebastian.barrio.b@gmail.com>
2021-06-11 18:20:44 -0500

    add header and banner

--- src/sections/banner.js
+++ src/sections/banner.js
@@ -7,7 +7,23 @@ import ShapeRight from 'assets/shape-right.png';
 
 export default function Banner() {
   return (
-    <h1>Banner</h1>
+    <section sx={styles.banner} id="home">
+      <Container sx={styles.banner.container}>
+        <Box sx={styles.banner.contentBox}>
+          <Heading as="h1" variant="heroPrimary">
+            Top Quality Digital Products to Explore
+          </Heading>
+          <Text as="P" variant="heroSecondary">
+            Check out our website to find great products and deals!
+            If you are looking for a great website this is the place!
+          </Text>
+          <Button variant="primary">Explore</Button>
+        </Box>
+        <Box sx={styles.banner.imageBox}>
+          <Image src={BannerImg} alt="banner"/>
+        </Box>
+      </Container>
+    </section>
   );
 }
 
