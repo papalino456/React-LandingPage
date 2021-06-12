86b0a567ea5f49c42f5ce32cf8fc9568829f2e4b
papalino456 <sebastian.barrio.b@gmail.com>
2021-06-11 19:09:13 -0500

    key feature and services

--- src/sections/service-section.js
+++ src/sections/service-section.js
@@ -4,7 +4,7 @@ import { jsx, Container, Box, Grid, Text, Heading, Button, Image } from 'theme-u
 import { keyframes } from '@emotion/core';
 import TextFeature from 'components/text-feature';
 import ModalVideo from 'react-modal-video';
-import { IoIosPlay } from 'react-icons/io';
+import { IoIosPlay, IoMdHeartEmpty } from 'react-icons/io';
 
 import ServiceThumb from 'assets/service-thumb.png';
 import shapePattern from 'assets/shape-pattern1.png';
@@ -36,8 +36,40 @@ const data = {
 };
 
 export default function ServiceSection() {
+  const handleClick = (e) => {
+    e.preventDefault();
+    setVideoOpen(true);
+  }
   return (
-    <h1>Service Section</h1>
+    <section sx={{variants: "section.services"}}>
+      <Container sx={styles.containerBox}>
+        <Box sx={styles.thumbnail}>
+          <Image src={ServiceThumb} alt="thumbnail"/>
+          <Button
+            sx={styles.videoBtn}
+            onClick={handleClick}
+            aria-label="Play Button"
+          >
+          <span>
+            <IoIosPlay/>
+          </span>
+          </Button>
+          <Box sx={styles.shapeBox}>
+            <Image src={shapePattern} alt="shape"/>
+          </Box>
+          <Box sx={styles.contentBox}>
+            <TextFeature subTitle={data.subTitle} title={data.title}/>
+            <Grid sx={styles.grid}>
+              {data.features.map((feature) => (
+                <Box sx={styles.card} key={feature.id}>
+                  <Image src={feature.imgSrc} alt={feature.altText} sx={styles.icon}/>
+                </Box>
+              ))}
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </section>
   );
 }
 
